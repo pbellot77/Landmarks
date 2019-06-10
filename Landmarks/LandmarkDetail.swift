859540c710dcfe561642e3fc6c9a661554648384
Patrick <patrickbellot77@gmail.com>
2019-06-10 17:19:24 -0500

    adding Landmark row

---
+++ Landmarks/LandmarkDetail.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Patrick Bellot on 6/4/19.
+//  Copyright © 2019 TwoTwenty8 LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+			VStack {
+				MapView()
+					.edgesIgnoringSafeArea(.top)
+					.frame(height: 300)
+				CircleImage()
+					.offset(y: -130)
+					.padding(.bottom, -130)
+				VStack(alignment: .leading){
+					Text("Turtle Rock")
+						.font(.title)
+					HStack(alignment: .bottom) {
+						Text("Joshua Tree National Park")
+							.font(.subheadline)
+						Spacer()
+						Text("California")
+							.font(.subheadline)
+					}
+				}
+				.padding()
+				
+				Spacer()
+			}
+    }
+}
+
+#if DEBUG
+struct LandmarkDetail_Previews : PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
+#endif
