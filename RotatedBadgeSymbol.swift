ab64a49a36d66f14132f64ca41454d698c09f7f0
Patrick <patrickbellot77@gmail.com>
2019-06-26 15:54:50 -0500

    Drawing Paths and Shapes sections 1-3

---
+++ RotatedBadgeSymbol.swift
@@ -0,0 +1,23 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Patrick Bellot on 6/26/19.
+//  Copyright © 2019 TwoTwenty8 LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct RotatedBadgeSymbol_Previews : PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol()
+    }
+}
+#endif
